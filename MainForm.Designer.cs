f616855c464e604866846d90051d05d3d2444372
shrayasr <shrayasr@gmail.com>
2015-09-26 23:36:23 +0530

    Add initial working version

---
+++ MainForm.Designer.cs
@@ -0,0 +1,75 @@
+﻿namespace WgetHere
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.urlTextBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // urlTextBox
+            // 
+            this.urlTextBox.Location = new System.Drawing.Point(42, 12);
+            this.urlTextBox.Name = "urlTextBox";
+            this.urlTextBox.Size = new System.Drawing.Size(431, 22);
+            this.urlTextBox.TabIndex = 0;
+            this.urlTextBox.KeyUp += new System.Windows.Forms.KeyEventHandler(this.urlTextBox_KeyUp);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(24, 17);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "url";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(485, 49);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.urlTextBox);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "MainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox urlTextBox;
+        private System.Windows.Forms.Label label1;
+
+    }
+}
+
---
+++ MainForm.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WgetHere
+{
+    public partial class MainForm : Form
+    {
+        // Where the application is called FROM
+        string _currentExecutingDirectory;
+
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            _currentExecutingDirectory = Directory.GetCurrentDirectory();
+        }
+
+        private void urlTextBox_KeyUp(object sender, KeyEventArgs e)
+        {
+            switch (e.KeyCode)
+            {
+                case Keys.Enter:
+                    string url = urlTextBox.Text.Trim().Replace("\"", "");
+                    string filenameFromURL = url.Substring(url.LastIndexOf('/') + 1);
+
+                    string destinationFilename = Path.Combine(_currentExecutingDirectory, filenameFromURL);
+
+                    WebClient webClient = new WebClient();
+                    webClient.DownloadFile(url, destinationFilename);
+
+                    Application.Exit();
+                    break;
+
+                case Keys.Escape:
+                    Application.Exit();
+                    break;
+
+                default:
+                    break;
+            }
+        }
+
+    }
+}
